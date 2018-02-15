d4570606e51ee6344169afeb1cd7df1c5d53d2ae
Gabriel Brasil <brasil.gabriel@gmail.com>
2018-02-15 15:31:13 -0500

    class Shawn update

--- week03/localserver.js
+++ week03/localserver.js
@@ -1,7 +1,9 @@
 
+//(2018 - update February 15) Config.js
 //(2018 - updated February 14) Adding Database
 //(2018 - updated February 13) Recreating the Express server exercise from DWD Servers class, but this time for localhosting. I'm comparing the originalr "server.js" with the "myapp/app.js" express file.
 
+var config = require("./config.js");
 var express = require ('express');
 var bodyParser = require ('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true});
@@ -12,7 +14,10 @@ var app = express();
 //Connect to mLab MongoDB database (after installing the node module)
 var mongojs = require('mongojs');
 //Bad idea to put your password in a file that's going to be sent to Github. Better use a config.js - but don't know how to access it!
-var db = mongojs("brasil.gabriel:Idunnodwd2018@ds235768.mlab.com:35768/dwd2018", ["DBtest00"]);
+// var db = mongojs("brasil.gabriel:Idunnodwd2018@ds235768.mlab.com:35768/dwd2018", ["DBtest00"]);
+
+var db = mongojs(config.db_username +":" + config.db_password + "@ds235768.mlab.com:35768/dwd2018", ["DBtest00"]);
+console.log(config.db_username);
 //mongodb://<dbuser>:<dbpassword>@ds235768.mlab.com:35768/dwd2018
 //var db = mongojs("username:password@example.com:port/mydb", ["mycollection"]);
 
@@ -20,11 +25,7 @@ app.listen(8080, function(){
   console.log('Local host server on port 8080...')
 })
 
-//DB: Insert a record into the Database.
-db.DBtest00.save({"movie":"back to the future"}, function(err, saved){
-    if( err || !saved) console.log("Not saved");
-    else console.log("saved");
-});
+
 
 //DB: Pull all records
 db.DBtest00.find({}, function(err, saved){
@@ -69,11 +70,16 @@ app.get('/templatetest', function (req, res){
 });
 
 //"post" request
-app.post('/processit', function(req, res) {
+app.post('/processit', function(req, res) { 
     var textvalue = req.body.textfield;
     var textvalue02 = req.body.textfield02;
     res.send("What make you think you are going to achieve " + textvalue +" ?!!"+ "And how dare you say " + textvalue02 +"? Not COOL!");
+    //DB: Insert a record into the Database. (update Shawn 0215: cut and pasted the "db.DBtest00.save..."" inside this "app.post")
+    db.DBtest00.save({"textfield":textvalue}, function(err, saved){
+        if( err || !saved) console.log("Not saved");
 
+        else console.log("saved");
+    });
   });
 
 
